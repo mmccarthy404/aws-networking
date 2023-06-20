3706229de0f679f6e98c874a68878fb8fc5b3c72
michael <michael.jos.mccarthy@gmail.com>
2023-06-19 21:17:00 -0400

    Fixed typo in variable CIDR name

--- vpc.tf
+++ vpc.tf
@@ -27,11 +27,11 @@ module "vpc" {
 
   cidr            = var.vpc.cidr
   azs             = slice(data.aws_availability_zones.available.names, 0, local.az_count)
-  private_subnets = [for i in range(local.az_count) : cidrsubnet(var.vpc_cidr, 8, i)]
-  public_subnets  = [for i in range(local.az_count, local.az_count * 2) : cidrsubnet(var.vpc_cidr, 8, i)]
+  private_subnets = [for i in range(local.az_count) : cidrsubnet(var.vpc.cidr, 8, i)]
+  public_subnets  = [for i in range(local.az_count, local.az_count * 2) : cidrsubnet(var.vpc.cidr, 8, i)]
 
   enable_nat_gateway  = true
   single_nat_gateway  = false
-  reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
+  reuse_nat_ips       = true             # Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = aws_eip.nat.*.id # IPs specified here as input to the module
 }
