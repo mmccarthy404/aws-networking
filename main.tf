51958676cfc4fa1739784a8859a0b5208dbeb329
Michael McCarthy <54035558+mmccarthy404@users.noreply.github.com>
2025-01-04 20:23:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_eip" "wireguard" {
 }
 
 module "wireguard" {
-  source        = "git::https://github.com/mmccarthy404/terraform-modules//terraform-aws-wireguard?ref=672ef55363e0bc5194776eba3f92366542570d6f" #v2.1.0
+  source        = "git::https://github.com/mmccarthy404/terraform-modules//terraform-aws-wireguard?ref=d6f5e426d617778ec41e7ff63e427478541e0dda" #v2.2.1
   instance_type = "t4g.nano"
   name          = "${local.name_prefix}-wireguard"
   subnet_id     = module.vpc.public_subnets[0]
