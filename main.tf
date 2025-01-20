9dd665ddb48367d3af72d734efa94f1e6f8a7165
Michael McCarthy <54035558+mmccarthy404@users.noreply.github.com>
2025-01-20 14:26:27 -0500

    Bugfix/wireguard routes (#25)
    * add WireGuard routes
    
    * add WireGuard routes
    
    * Update main.tf
    
    * Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_eip" "wireguard" {
 }
 
 module "wireguard" {
-  source        = "git::https://github.com/mmccarthy404/terraform-modules//terraform-aws-wireguard?ref=d6f5e426d617778ec41e7ff63e427478541e0dda" #v2.2.1
+  source        = "git::https://github.com/mmccarthy404/terraform-modules//terraform-aws-wireguard?ref=7262f775270c0e0f8499b5ae51dba30da41d7cca" #v2.2.4
   instance_type = "t4g.nano"
   name          = "${local.name_prefix}-wireguard"
   subnet_id     = module.vpc.public_subnets[0]
@@ -80,4 +80,12 @@ module "wireguard" {
   wireguard_peer_source_ip        = data.aws_ssm_parameter.wireguard_peer_source_ip.value
 
   tags = var.tags
+}
+
+resource "aws_route" "wireguard" {
+  for_each = toset(module.vpc.private_route_table_ids)
+
+  route_table_id         = each.value
+  destination_cidr_block = module.wireguard.wireguard_cidr
+  network_interface_id   = module.wireguard.network_interface.id
 }
